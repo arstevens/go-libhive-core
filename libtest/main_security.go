2f7847f9e4a3587066535edda6362e06a1c8623f
Aleksandr Stevens <aleksandr@pop-os.localdomain>
2020-01-03 15:09:46 -0800

    stuff

--- libtest/main_security.go
+++ libtest/main_security.go
@@ -4,11 +4,9 @@ import (
 	"log"
 
 	security "github.com/arstevens/go-libhive-core/security"
-	ipfsapi "github.com/ipfs/go-ipfs-api"
 )
 
 func main() {
-	sh := ipfsapi.NewLocalShell()
 	_, err := security.RetrieveLocalPrivateKey()
 	if err != nil {
 		log.Fatal(err)
