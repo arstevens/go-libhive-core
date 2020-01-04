d388f79a966d67d947de44526de1f65382175ea7
Aleksandr Stevens <aleksandr@pop-os.localdomain>
2020-01-03 16:28:13 -0800

    stuff

--- message/header.go
+++ message/header.go
@@ -85,6 +85,10 @@ func (m *MessageHeader) MsgId() int {
 
 func (m *MessageHeader) DataLen() int {
 	dLen, ok := m.header[DataLenField].(int)
+	fmt.Println(m.header[DataLenField])
+	dLen2, ok := m.header[DataLenField].(string)
+	fmt.Println(ok)
+	fmt.Println(dLen2)
 	if !ok {
 		fmt.Println("Could not assert DataLenField to Int in Message")
 		return -1
